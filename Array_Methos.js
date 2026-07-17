db49d17108fe4aa947bc3082e015f0cd3de73c9f
Shivanshu singh <152587989+0shivanshuu@users.noreply.github.com>
2026-07-17 06:14:26 +0000

    Array Methods

---
+++ Array_Methos.js
@@ -0,0 +1,26 @@
+let primary=["red", "Yellow", "Green"];
+
+let secondary=["orange", "blue", "violet"];
+
+let allcolor= primary.concat(secondary);
+
+let reverseP=primary.reverse();
+
+console.log(allcolor);
+console.log(reverseP);
+
+// Splice method
+
+let array=['january','july','march','august'];
+
+array.splice(0,2,'july','june');
+
+console.log(array);
+
+// Indexof Method
+
+let array2=['java','sql','cpp','python','javascript','go','lang','css']
+
+let index=array2.reverse().indexof('javascript');
+
+console.log(index);
