15067cee7b9b709b82da2d0880dd660baf498a96
Shivanshu singh <152587989+0shivanshuu@users.noreply.github.com>
2026-07-17 05:37:02 +0000

    Concate and reverse array method

---
+++ concat_reverse.js
@@ -0,0 +1,10 @@
+let primary=["red", "Yellow", "Green"];
+
+let secondary=["orange", "blue", "violet"];
+
+let allcolor= primary.concat(secondary);
+
+let reverseP=primary.reverse();
+
+console.log(allcolor);
+console.log(reverseP);
